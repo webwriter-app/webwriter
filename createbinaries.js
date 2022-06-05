c6e42458dccf886e5f0c238d0b0128f3dc15734c
Frederic Salmen <frederic@fsalmen.de>
2022-06-05 21:43:14 +0200

    Add esbuild to binaries

--- createbinaries.js
+++ createbinaries.js
@@ -1,20 +1,27 @@
 /*
-Copies and renames npm binaries made with pkg for all supported platforms (Windows, Mac OS, Linux) with Rust style triples specifying the compilation target, so Tauri can use npm as a sidecar.
+Copies and renames binaries for all supported platforms (Windows, Mac OS, Linux) with Rust style triples specifying the compilation target, so Tauri can use npm as a sidecar.
 */
 
 const fs = require('fs')
 const path = require('path')
 const child_process = require("child_process")
 
-"echo {\"scripts\": \"lib/**/*\"} > ./node_modules/npm/pkg.json && node ./node_modules/pkg/lib-es5/bin.js ./node_modules/npm/index.js --config ./node_modules/npm/pkg.json --targets node16-win-x64,node16-macos-x64,node16-linux-x64,node16-win-arm64,node16-macos-arm64,node16-linux-arm64 --no-bytecode --public-packages \"*\" --public --output ./binaries/npm"
-
-const TRIPLES_OF_PACKAGES = {
+const TRIPLES_OF_NPM = {
   "npm-win-x64": "x86_64-pc-windows-msvc",
   "npm-win-arm64": "arm64-pc-windows-msvc",
+  "npm-macos-x64": "x86_64-apple-darwin",
+  "npm-macos-arm64": "arm64-apple-darwin",
   "npm-linux-x64": "x86_64-unknown-linux-gnu",
   "npm-linux-arm64": "arm64-unknown-linux-gnu",
-  "npm-macos-x64": "x86_64-apple-darwin",
-  "npm-macos-arm64": "arm64-apple-darwin"
+}
+
+const TRIPLES_OF_ESBUILD = {
+  "esbuild-windows-64": "x86_64-pc-windows-msvc",
+  "esbuild-windows-arm64": "arm64-pc-windows-msvc",
+  "esbuild-darwin-64": "x86_64-apple-darwin",
+  "esbuild-darwin-arm64": "arm64-apple-darwin",
+  "esbuild-linux-64": "x86_64-unknown-linux-gnu",
+  "esbuild-linux-arm64": "arm64-unknown-linux-gnu",
 }
 
 const PKG_PATH = path.normalize("./node_modules/pkg/lib-es5/bin.js")
@@ -28,19 +35,30 @@ const PKG_EXTRA_FLAGS = "--no-bytecode --public-packages \"*\" --public"
 
 async function main() {
 
+  const binariesDir = "./binaries"
+  !fs.existsSync(binariesDir)? fs.mkdirSync(binariesDir): null
+
   fs.writeFileSync(`${PKG_CONFIG_PATH}`, PKG_CONFIG)
 
-
   child_process.execSync(`node ${PKG_PATH} ${NPM_ENTRYPOINT} --config ${PKG_CONFIG_PATH} --targets ${PKG_TARGETS} --output ${PKG_OUTPUT} ${PKG_EXTRA_FLAGS}`, {encoding: "utf8"})
- 
-  for(const [binname, triple] of Object.entries(TRIPLES_OF_PACKAGES)) {
+
+  for(const [binname, triple] of Object.entries(TRIPLES_OF_NPM)) {
     const suffix = binname.includes("win")? ".exe": ""
-    const binariesDir = "./binaries"
-    !fs.existsSync(binariesDir)? fs.mkdirSync(binariesDir): null
     fs.renameSync(
       `./binaries/${binname}${suffix}`,
       `./binaries/npm-${triple}${suffix}`
     )
+  }
+
+  for(const [packagename, triple] of Object.entries(TRIPLES_OF_ESBUILD)) {
+    const prefix = !packagename.includes("windows")? "bin/": ""
+    const suffix = packagename.includes("windows")? ".exe": ""
+    if(fs.existsSync(`./node_modules/${packagename}`)) {
+      fs.copyFileSync(
+        `./node_modules/${packagename}/${prefix}esbuild${suffix}`,
+        `./binaries/esbuild-${triple}${suffix}`
+      )
+    }
   }
 }
 
