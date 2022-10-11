cef2cb5032c10dbde8025e8492dc603eeb30993a
Frederic Salmen <frederic@fsalmen.de>
2022-10-11 02:43:23 +0200

    Bundle icons properly in workflow

--- scripts/preparestatic.js
+++ scripts/preparestatic.js
@@ -10,11 +10,13 @@ const process = require("process")
 
 const STATIC_PATH = path.normalize(process.argv[2] ?? "./static")
 const ICONS_PATH = path.normalize("./node_modules/@shoelace-style/shoelace/dist/assets/icons/")
+const CC_PATH = path.normalize("./static/assets/cc/")
 const STATIC_ICONS_PATH = path.join(STATIC_PATH, "assets", "icons")
 
 async function main() {
   !existsSync(STATIC_PATH) && mkdirSync(STATIC_PATH)
   fse.copySync(ICONS_PATH, STATIC_ICONS_PATH)
+  fse.copySync(CC_PATH, STATIC_ICONS_PATH)
 }
 
 main().catch((e) => {
