644a3d3f987731bf28f0302ce122b806b23cd1c5
Frederic Salmen <frederic@fsalmen.de>
2024-01-21 22:10:32 +0100

    Allow arbitrary properties on webwriterpackage

--- scripts/generatejsonschema.ts
+++ scripts/generatejsonschema.ts
@@ -2,7 +2,11 @@ import {Package} from "../@webwriter/core/model/schemas/packageschema"
 import { zodToJsonSchema } from "zod-to-json-schema"
 import {writeFileSync} from "fs"
 
-const schemaObj = zodToJsonSchema(Package.coreObjectSchema)
+const schemaObj = {
+  ...zodToJsonSchema(Package.coreObjectSchema, "webwriterpackage"),
+  $schema: "https://json.schemastore.org/package.json",
+  additionalProperties: true
+}
 const schemaStr = JSON.stringify(schemaObj, undefined, 2)
 writeFileSync("./webwriterpackage.json", schemaStr, "utf8")
 
