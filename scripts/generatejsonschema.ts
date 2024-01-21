c3481a0df7af84fd043dd85af06ac7022a0d6c37
Frederic Salmen <frederic@fsalmen.de>
2024-01-21 22:18:48 +0100

    Fix additionalproperties

--- scripts/generatejsonschema.ts
+++ scripts/generatejsonschema.ts
@@ -2,11 +2,9 @@ import {Package} from "../@webwriter/core/model/schemas/packageschema"
 import { zodToJsonSchema } from "zod-to-json-schema"
 import {writeFileSync} from "fs"
 
-const schemaObj = {
-  ...zodToJsonSchema(Package.coreObjectSchema, "webwriterpackage"),
-  $schema: "https://json.schemastore.org/package.json",
-  additionalProperties: true
-}
+const schemaObj = zodToJsonSchema(Package.coreObjectSchema, "webwriterpackage")
+schemaObj.$schema = "https://json.schemastore.org/package.json";
+(schemaObj as any).definitions.webwriterpackage.additionalProperties = true
 const schemaStr = JSON.stringify(schemaObj, undefined, 2)
 writeFileSync("./webwriterpackage.json", schemaStr, "utf8")
 
