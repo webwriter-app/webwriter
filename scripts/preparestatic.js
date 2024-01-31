a221ea1f444ae13a83b0c961026ed4ccf27dd532
Frederic Salmen <frederic@fsalmen.de>
2024-01-31 19:17:56 +0100

    Fix missing custom icon

--- scripts/preparestatic.js
+++ scripts/preparestatic.js
@@ -11,12 +11,15 @@ const process = require("process")
 const STATIC_PATH = path.normalize(process.argv[2] ?? "./static")
 const ICONS_PATH = path.normalize("./node_modules/@tabler/icons/icons")
 const CC_PATH = path.normalize("./node_modules/@wagnerflo/cc-icons/fonts/cc-icons-svg/")
+const ASSETS_PATH = path.normalize("./@webwriter/assets/")
+const STATIC_ASSETS_PATH = path.join(STATIC_PATH, "assets")
 const STATIC_ICONS_PATH = path.join(STATIC_PATH, "assets", "icons")
 
 async function main() {
   !existsSync(STATIC_ICONS_PATH) && mkdirSync(STATIC_ICONS_PATH, {recursive: true})
   fse.copySync(ICONS_PATH, STATIC_ICONS_PATH)
   fse.copySync(CC_PATH, STATIC_ICONS_PATH)
+  fse.copySync(ASSETS_PATH, STATIC_ASSETS_PATH)
 }
 
 main().catch((e) => {
