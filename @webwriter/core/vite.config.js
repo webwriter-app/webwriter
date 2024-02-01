d83ae0916030a3176475c909c8dba5c9abeef580
Frederic Salmen <frederic@fsalmen.de>
2024-02-01 09:36:19 +0100

    Fix settings window missing in production

--- @webwriter/core/vite.config.js
+++ @webwriter/core/vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
         rollupOptions: {
           input: {
             app: fileURLToPath(new URL("./index.html", import.meta.url)),
+            settings: fileURLToPath(new URL("./settings.html", import.meta.url))
           }
         }
     }
