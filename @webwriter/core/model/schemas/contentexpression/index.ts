d49e0327041adac94197b1803be372b376ce13d0
Frederic Salmen <frederic@fsalmen.de>
2024-05-06 23:02:30 +0200

    Refactor content expression parser with lezer

--- @webwriter/core/model/schemas/contentexpression/index.ts
+++ @webwriter/core/model/schemas/contentexpression/index.ts
@@ -1,7 +1,74 @@
 import {z} from "zod"
 import {Schema} from "prosemirror-model"
-//@ts-ignore
-import { parse } from "./parse"
+
+import {parser} from "./index.grammar"
+
+function treeLog(tree: any, expr?: string) {
+  let depth = -1
+  tree.cursor().iterate(
+    (node: any) => {
+      depth++
+      console.group(`${node.name}[${node.from}:${node.to}|${depth}]`, expr?.slice(node.from, node.to))
+    },
+    () => {
+      depth--
+      console.groupEnd()
+    }
+  )
+}
+
+const expr = "(a* | b  c)+"
+
+function parse(expr: string) {
+  const tree = parser.parse(expr)
+  let root: any = undefined
+  let parent: any = undefined
+  tree.cursor().iterate(
+    (cur: any) => {
+      if(["Alternation", "Sequence", "SimpleExpression"].includes(cur.name)) {
+        const value = {type: cur.name, min: 1, max: 1, content: [], parent, raw: expr.slice(cur.from, cur.to)}
+
+        if(parent) {
+          Object.assign(parent, {content: [...parent?.content, value]})
+          parent = value
+        }
+        else {
+          root = parent = value
+        }
+      }
+      else if(cur.name === "Identifier") {
+        Object.assign(parent, {content: expr.slice(cur.from, cur.to)})
+      }
+      else if(cur.name === "ZeroOrOne") {
+        Object.assign(parent, {min: 0, max: 1})
+      }
+      else if(cur.name === "ZeroOrMore") {
+        Object.assign(parent, {min: 0, max: Infinity})
+      }
+      else if(cur.name === "OneOrMore") {
+        Object.assign(parent, {min: 1, max: Infinity})
+      }
+      else if(cur.name === "Min") {
+        Object.assign(parent, {min: parseInt(expr.slice(cur.from, cur.to))})
+      }
+      else if(cur.name === "Max") {
+        Object.assign(parent, {max: parseInt(expr.slice(cur.from, cur.to))})
+      }
+    },
+    (cur: any) => {
+      const isComplex = ["Alternation", "Sequence", "SimpleExpression"].includes(cur.name)
+      const isInGroup = cur.node.parent?.name === "Group"
+      const isGroup = cur.name === "Group"
+      if(isComplex && !isInGroup) {
+        parent = parent?.parent
+      }
+      else if(isGroup) {
+        parent = parent?.parent
+      }
+    }
+  )
+  return root
+}
 
 export type Expression = SimpleExpression | Sequence | Alternation | NodeExpression
 
@@ -33,13 +100,13 @@ export interface Alternation extends BaseExpression {
   content: Expression[]
 }
 
-
 export type ContentExpression = z.infer<typeof ContentExpression>
 export const ContentExpression = Object.assign(z.string().transform<Expression>(arg => parse(arg)), {
   serialize(value: Expression) {
     return value.raw
   },
-  resolve(schema: Schema, expression: ParentedExpression, parent?: ParentedExpression): ParentedExpression {
+  resolve(schema: Schema, expression: ParentedExpression, parent?: ParentedExpression, seen=new Map()): ParentedExpression {
+    console.log(expression.raw)
     const {content} = expression
     const representants = {} as Record<string, string>
     schema.spec.nodes.forEach((k, v) => {
@@ -59,8 +126,9 @@ export const ContentExpression = Object.assign(z.string().transform<Expression>(
         node: node.name,
         parent
       }
+      seen.set(expression.raw, result)
       const nodeContent = node.spec.content
-        ? ContentExpression.resolve(schema, ContentExpression.parse(node.spec.content), result as any)
+        ? seen.get(node.spec.content) ?? ContentExpression.resolve(schema, ContentExpression.parse(node.spec.content), result as any, seen)
         : undefined
       return Object.assign(result, {content: nodeContent? [nodeContent]: undefined}) as any
     }
@@ -69,7 +137,8 @@ export const ContentExpression = Object.assign(z.string().transform<Expression>(
         ...expression as Exclude<Expression, SimpleExpression>,
         parent
       }
-      return Object.assign(result, {content: content.map(c => ContentExpression.resolve(schema, c, result))})
+      seen.set(expression.raw, result)
+      return seen.get(expression.raw) ?? Object.assign(result, {content: content.map(c => ContentExpression.resolve(schema, c, result, seen))})
     }
     else {
       return expression
