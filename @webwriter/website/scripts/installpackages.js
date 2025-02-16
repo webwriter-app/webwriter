f1135d6e2a77d7efc186e7af11d8d5f47eb8ad4f
Frederic Salmen <4583669+salmenf@users.noreply.github.com>
2025-02-16 16:17:51 +0100

    Rework Website for WebWriter 1.0

---
+++ @webwriter/website/scripts/installpackages.js
@@ -0,0 +1,108 @@
+import fs from "fs-extra"
+import {execSync} from "child_process"
+import path from "path"
+
+const REGISTRY = "https://registry.npmjs.org"
+const REGISTRY_SEARCH = `${REGISTRY}/-/v1/search`
+
+const dirSize = async directory => {
+    const files = await fs.readdir( directory );
+    const stats = files.map( file => fs.stat( path.join( directory, file ) ) );
+  
+    return ( await Promise.all( stats ) ).reduce( ( accumulator, { size } ) => accumulator + size, 0 );
+  }
+
+const response = await fetch(REGISTRY_SEARCH + "?" + new URLSearchParams({
+   text: "keywords:webwriter-widget",
+   size: 250 
+}))
+
+const body = await response.json()
+
+const pkgResults = body.objects
+  .map(obj => obj.package)
+  .filter(pkg => pkg.name.startsWith("@webwriter/"))
+
+const pkgs = (await Promise.allSettled(pkgResults.map(async result => {
+  const resp = await fetch(`https://cdn.jsdelivr.net/npm/${result.name}@${result.version}/package.json`)
+  const pkg = JSON.parse(await resp.text())
+  const snippetKeys = Object.keys(pkg.exports).filter(k => k.startsWith("./snippets/"))
+  pkg.snippets = {}
+  for(const k of snippetKeys) {
+    try {
+      const url = pkg.exports[k]?.default ?? pkg.exports[k]
+      const snippetResp = await fetch(`https://cdn.jsdelivr.net/npm/${pkg.name}@${pkg.version}/${url.slice(2)}`)
+      const snippet = await snippetResp.text()
+      pkg.snippets[k] = snippet
+    }
+    catch(err) {
+      console.error(err)
+    }
+  }
+  return pkg
+  })))
+  .map(settled => {
+    if(settled.status === "rejected") {
+      console.warn(settled.reason)
+      return undefined
+    }
+    else {
+      return settled.value
+    }
+  })
+  .filter(pkg => pkg)
+
+fs.writeFileSync("resources/packages.json", JSON.stringify(pkgs, undefined, 2), "utf8")
+
+/*
+
+try {
+  console.log(execSync(`npm install`, {cwd: "./public/widgetsrc"}).toString())
+}
+catch(err) {
+  throw err
+}
+
+*/
+
+/*
+const importStatements = Object.fromEntries(names.map(name => [
+    name,
+    `import "redefine-custom-elements"; import "${name}"`
+]))
+
+const esbuildPath = path.normalize("public/widgetsrc/node_modules/.bin/esbuild")
+
+const tsconfigPath = path.normalize("tmp/tsconfig.json")
+fs.ensureFileSync(tsconfigPath)
+fs.writeFileSync(tsconfigPath, `{
+  "compilerOptions": {
+    "experimentalDecorators": true
+  }
+}
+`, {encoding: "utf8"})
+
+const bundleSizes = []
+const installSizes = []
+for(const [i, name] of names.entries()) {
+    const jsFile = `tmp/${name}.js`
+    fs.ensureFileSync(jsFile)
+    fs.writeFileSync(jsFile, importStatements[name], {encoding: "utf8"})
+    const outFileJs = `public/widgetsrc/${name}.js`
+    const outFileCss = outFileJs.slice(0, -3) + ".css"
+    try {
+      execSync(`${esbuildPath} --bundle ${jsFile} --tsconfig=tmp/tsconfig.json --outfile=${outFileJs} --minify`, {env: {"NODE_PATH": "public/widgetsrc/node_modules"}})
+    }
+    catch(err) {
+      console.error(err)
+      continue
+    }
+    const js = fs.statSync(outFileJs).size
+    const css = fs.existsSync(outFileCss)? fs.statSync(outFileCss).size: 0
+    const installSize = await dirSize(`public/widgetsrc/node_modules/${name}`)
+    bundleSizes.push({js, css})
+    installSizes.push(installSize)
+}
+
+// fs.rm("tmp", {recursive: true})
+*/
---
+++ @webwriter/website/src/components/impressum.vue
@@ -0,0 +1,42 @@
+<template>
+<div id="text">
+<h1>Legal Notice</h1>
+
+<div>
+<u>Responsible:</u><br>
+{{name}}<br>
+{{address}}<br>
+Email: {{local}}@{{domain}}<br>
+Phone: {{phone}}<br>
+</div>
+
+<p>Online Dispute Resolution: <a href="https://ec.europa.eu/consumers/odr">https://ec.europa.eu/consumers/odr</a></p>
+
+<p>This Legal Notice complies with the German laws under § 5 TMG and § 55 RStV.</p>
+ 
+
+<h2>Liability for Content</h2>
+
+<p>The contents of our website have been created with the greatest possible care. However, we cannot guarantee the contents' accuracy, completeness, or topicality. According to Section 7, paragraph 1 of the TMG (Telemediengesetz - German Telemedia Act), we as service providers are liable for our content on these pages by general laws. However, according to Sections 8 to 10 of the TMG, we service providers are not obliged to monitor external information transmitted or stored or investigate circumstances pointing to illegal activity. Obligations to remove or block the use of information under general laws remain unaffected. However, a liability in this regard is only possible from the moment of knowledge of a specific infringement. Upon notification of such violations, we will remove the content immediately.</p>
+
+<h2>Liability for Links</h2>
+
+<p>Our website contains links to external websites, over whose contents we have no control. Therefore, we cannot accept any liability for these external contents. The respective provider or operator of the websites is always responsible for the contents of the linked pages. The linked pages were checked for possible legal violations at the time of linking. Illegal contents were not identified at the time of linking. However, permanent monitoring of the contents of the linked pages is not reasonable without specific indications of a violation. Upon notification of violations, we will remove such links immediately.</p>
+
+<h2>Copyright</h2>
+
+<p>The contents and works on these pages created by the site operator are subject to German copyright law. The duplication, processing, distribution, and any kind of utilization outside the limits of copyright require the written consent of the respective author or creator. Downloads and copies of these pages are only permitted for private, non-commercial use. In so far as the contents on this site were not created by the operator, the copyrights of third parties are respected. In particular, third-party content is marked as such. Should you become aware of a copyright infringement, please inform us accordingly. Upon notification of violations, we will remove such contents immediately.</p>
+
+<i>Last Updated: May 29, 2023</i>
+</div>
+</template>
+  
+<script setup lang="ts">
+  const props = defineProps({
+    name: String,
+    address: String,
+    local: String,
+    domain: String,
+    phone: String
+  })
+</script>
